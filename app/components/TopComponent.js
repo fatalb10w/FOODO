418a5d4e19be97d37fb9a20b5d23e31d9872a7fa
D1S3CT <Deifordhaze@gmail.com>
2025-03-17 21:40:32 +0400

    first commit

---
+++ app/components/TopComponent.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+function TopComponent() {
+  return (
+    <>
+       <div className='background'></div>
+       <div className='texture'>
+            <Container className='container-top'>
+              <Row className='section-top'>
+                <Col xs={12} md={6} className='section-top-left'>
+                  <img className='img-fluid' src='/assets/mobile.png' />
+                </Col>
+                <Col xs={12} md={6} className='section-top-right'>
+                  <div className='plate-img-container'></div>
+                  <h6>FOODO</h6>
+                  <h2>Get Your Food In</h2>
+                  <h1>FOODO</h1>
+                  <p>Laborum ipsum ex labore ex laborum aliqua fugiat fugiat esse culpa. Aliquip
+                    sint est sunt in exercitation deserunt aute.</p>
+                 <button className='btn btn-custom'>Download APP</button>
+                </Col>
+              </Row>
+            </Container>
+       </div>
+    </>
+  )
+}
+
+export default TopComponent
