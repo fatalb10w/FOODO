2831d532f46dd2fa7e2f03145f659191571a6ce0
D1S3CT <Deifordhaze@gmail.com>
2025-03-18 22:09:42 +0400

    Made to Perfect!

--- app/components/TopComponent.js
+++ app/components/TopComponent.js
@@ -6,20 +6,30 @@ function TopComponent() {
     <>
        <div className='background'></div>
        <div className='texture'>
-            <Container className='container-top'>
+            <Container className="container-top no-padding-container">
               <Row className='section-top'>
                 <Col xs={12} md={6} className='section-top-left'>
                   <img className='img-fluid' src='/assets/mobile.png' />
                 </Col>
-                <Col xs={12} md={6} className='section-top-right shadow'>
-                  <div className='plate-img-container'></div>
-                  <h6>FOODO</h6>
-                  <h2>Get Your Food In</h2>
-                  <h1>FOODO</h1>
-                  <p>Laborum ipsum ex labore ex laborum aliqua fugiat fugiat esse culpa. Aliquip
-                    sint est sunt in exercitation deserunt aute.</p>
-                 <button className='btn btn-custom'>Download APP</button>
-                </Col>
+                <Col xs={12} md={6} 
+                    className='section-top-right shadow p-4 d-flex flex-column gap-3 text-md-start text-center no-padding-mobile'>
+                    <div className='plate-img-container'></div>
+
+                    <h6 className="text-uppercase fw-bold">FOODO</h6>
+
+                    <h2 className="responsive-text">Get Your Food In</h2>
+                    <h1 className="responsive-title">FOODO</h1>
+
+                    <p className="responsive-paragraph">
+                      Laborum ipsum ex labore ex laborum aliqua fugiat fugiat esse culpa. 
+                      Aliquip sint est sunt in exercitation deserunt aute.
+                    </p>
+
+                    <div className="d-flex justify-content-md-start justify-content-center">
+                      <button className='btn btn-custom'>Download APP</button>
+                    </div>
+                  </Col>
+
               </Row>
             </Container>
        </div>
